5bbc3e4a80d26e8e61233977a84bc67d3f694144
DanielSaborido <dsaborio@iesrafaelalberti.es>
2023-04-30 21:10:09 +0200

    refactorizacion de la implementacion de productos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,35 +1,21 @@
 import entities.Products
 
 fun main() {
-    val product1 = Products(name = "Camisa", description = "Camisa de algodón", price = 20.0f, taxes = 19, stock = 50)
-    val product2 = Products(name = "Pantalón", description = "Pantalón de mezclilla", price = 30.0f, taxes = 19, stock = 30)
-    val product3 = Products(name = "Zapatos", description = "Zapatos de piel", price = 50.0f, taxes = 19, stock = 10)
-    val product4 = Products(name = "Gorra", description = "Gorra deportiva", price = 10.0f, taxes = 19, stock = 100)
-    val product5 = Products(name = "Calcetines", description = "Calcetines de algodón", price = 5.0f, taxes = 19, stock = 200)
-    val product6 = Products(name = "Vestido", description = "Vestido de seda", price = 80.0f, taxes = 19, stock = 20)
-    val product7 = Products(name = "Traje de baño", description = "Traje de baño para mujer", price = 25.0f, taxes = 19, stock = 40)
-    val product8 = Products(name = "Bolso", description = "Bolso de cuero", price = 60.0f, taxes = 19, stock = 15)
-    val product9 = Products(name = "Reloj", description = "Reloj de pulsera", price = 100.0f, taxes = 19, stock = 5)
-    val product10 = Products(name = "Lentes de sol", description = "Lentes de sol polarizados", price = 35.0f, taxes = 19, stock = 30)
     val myDataSource = DataSourceImpl()
-    var productId = CProducts(myDataSource).create(product1)
-    println("Product ID: $productId")
-    productId = CProducts(myDataSource).create(product2)
-    println("Product ID: $productId")
-    productId = CProducts(myDataSource).create(product3)
-    println("Product ID: $productId")
-    productId = CProducts(myDataSource).create(product4)
-    println("Product ID: $productId")
-    productId = CProducts(myDataSource).create(product5)
-    println("Product ID: $productId")
-    productId = CProducts(myDataSource).create(product6)
-    println("Product ID: $productId")
-    productId = CProducts(myDataSource).create(product7)
-    println("Product ID: $productId")
-    productId = CProducts(myDataSource).create(product8)
-    println("Product ID: $productId")
-    productId = CProducts(myDataSource).create(product9)
-    println("Product ID: $productId")
-    productId = CProducts(myDataSource).create(product10)
-    println("Product ID: $productId")
+    val productList = listOf(
+        Products(name = "Camisa", description = "Camisa de algodón", price = 20.0f, taxes = 19, stock = 50),
+        Products(name = "Pantalón", description = "Pantalón de mezclilla", price = 30.0f, taxes = 19, stock = 30),
+        Products(name = "Zapatos", description = "Zapatos de piel", price = 50.0f, taxes = 19, stock = 10),
+        Products(name = "Gorra", description = "Gorra deportiva", price = 10.0f, taxes = 19, stock = 100),
+        Products(name = "Calcetines", description = "Calcetines de algodón", price = 5.0f, taxes = 19, stock = 200),
+        Products(name = "Vestido", description = "Vestido de seda", price = 80.0f, taxes = 19, stock = 20),
+        Products(name = "Traje de baño", description = "Traje de baño para mujer", price = 25.0f, taxes = 19, stock = 40),
+        Products(name = "Bolso", description = "Bolso de cuero", price = 60.0f, taxes = 19, stock = 15),
+        Products(name = "Reloj", description = "Reloj de pulsera", price = 100.0f, taxes = 19, stock = 5),
+        Products(name = "Lentes de sol", description = "Lentes de sol polarizados", price = 35.0f, taxes = 19, stock = 30)
+    )
+    for (product in productList) {
+        val productId = CProducts(myDataSource).create(product)
+        println("Product ID: $productId")
+    }
 }
